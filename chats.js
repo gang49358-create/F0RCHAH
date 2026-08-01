077bd17ffa6c3af2fefd4bdcb6b8442d22478227
gang49358-create <gang49358@gmail.com>
2026-08-02 08:21:10 +1000

    Create chats.js

---
+++ chats.js
@@ -0,0 +1,31 @@
+const chats = [
+
+{
+id:1,
+name:"Alex",
+avatar:"A",
+last:"Привет 👋"
+},
+
+{
+id:2,
+name:"Dark Premium",
+avatar:"⭐",
+last:"Добро пожаловать в Premium"
+},
+
+{
+id:3,
+name:"Gaming Squad",
+avatar:"🎮",
+last:"Кто играет?"
+},
+
+{
+id:4,
+name:"Новости",
+avatar:"N",
+last:"Последние новости"
+}
+
+];
