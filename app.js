24ebf5781f5bbd2a635b76290a90fc463f43b988
gang49358-create <gang49358@gmail.com>
2026-08-02 05:51:16 +1000

    Update app.js

--- app.js
+++ app.js
@@ -64,3 +64,52 @@ window.location.href="index.html";
 
 
 }
+function sendMessage(){
+
+let input =
+document.getElementById("messageInput");
+
+
+let text = input.value;
+
+
+if(text=="") return;
+
+
+let messages =
+document.getElementById("messages");
+
+
+let div =
+document.createElement("div");
+
+
+div.className="message";
+
+
+div.innerText=text;
+
+
+messages.appendChild(div);
+
+
+input.value="";
+
+
+}
+
+
+
+function openChat(){
+
+window.location.href="chat.html";
+
+}
+
+
+
+function back(){
+
+window.location.href="chats.html";
+
+}
