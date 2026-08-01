b5268ffa25dabadc5e1024e9255cb82006a5b6c0
gang49358-create <gang49358@gmail.com>
2026-08-02 07:25:23 +1000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,33 +1,42 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 
 import { 
 getAuth 
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+} from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 
 import { 
 getFirestore 
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+} from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+
+
+// ВСТАВЬ СЮДА СВОЙ FIREBASE CONFIG
 
 const firebaseConfig = {
 
-apiKey: "AIzaSyASNOjPhKOw3WWo1xew6oa09pbIkjzbqeE",
+apiKey: "ТВОЙ_API_KEY",
 
-authDomain: "darkweb-8acc4.firebaseapp.com",
+authDomain: "ТВОЙ_PROJECT.firebaseapp.com",
 
-projectId: "darkweb-8acc4",
+projectId: "ТВОЙ_PROJECT_ID",
 
-storageBucket: "darkweb-8acc4.firebasestorage.app",
+storageBucket: "ТВОЙ_PROJECT.appspot.com",
 
-messagingSenderId: "957568283427",
+messagingSenderId: "ТВОЙ_SENDER_ID",
 
-appId: "1:957568283427:web:157087de0403e69dcb5296"
+appId: "ТВОЙ_APP_ID"
 
 };
 
 
+
 const app = initializeApp(firebaseConfig);
+
 
 
 export const auth = getAuth(app);
