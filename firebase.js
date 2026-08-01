02903b9ca746dbbfec02c232a303a8f77f8850af
gang49358-create <gang49358@gmail.com>
2026-08-02 06:59:48 +1000

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,35 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { 
+getAuth 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+import { 
+getFirestore 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyASNOjPhKOw3WWo1xew6oa09pbIkjzbqeE",
+
+authDomain: "darkweb-8acc4.firebaseapp.com",
+
+projectId: "darkweb-8acc4",
+
+storageBucket: "darkweb-8acc4.firebasestorage.app",
+
+messagingSenderId: "957568283427",
+
+appId: "1:957568283427:web:157087de0403e69dcb5296"
+
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
